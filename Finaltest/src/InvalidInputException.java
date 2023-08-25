1d7475403cf8b025fd28e241f4a120d61e739002
YnSWen <124570686+YnSWen@users.noreply.github.com>
2023-08-25 21:10:23 +0300

    Delete Finaltest directory

--- Finaltest/src/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Класс для создания собственного исключения - некорректного ввода
- */
-public class InvalidInputException extends Exception {
-    public InvalidInputException (String msg){
-        super(msg);
-    }
-}
